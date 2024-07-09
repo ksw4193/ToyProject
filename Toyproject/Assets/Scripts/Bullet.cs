1dd34fd8708aef36fb40293b88f800e7bd273d35
whj0623 <whj062379@gmail.com>
2024-07-09 17:07:23 +0900

    3

--- Toyproject/Assets/Scripts/Bullet.cs
+++ Toyproject/Assets/Scripts/Bullet.cs
@@ -1,3 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
+public class Bullet : MonoBehaviour
+{
+    public float damage = 10;
+
+    private void Start()
+    {
+        Destroy(gameObject, 10);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Drone>(out Drone drone))
+        {
+            drone.GetDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
